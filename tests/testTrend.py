674241cda4937973b6422e11574133e7edd176b2
hoytdarnell437-source <hoytdarnell437@gmail.com>
2026-02-12 10:50:45 -0500

    add second testing file using different strategy

--- tests/testTrend.py
+++ tests/testTrend.py
@@ -18,11 +18,8 @@ while True:
     highPrices = data["High"][stockSymbol].tolist()
     lowPrices = data["Low"][stockSymbol].tolist()
 
-
-    numberShares = 1
-
-    startingCapital = 100
-    capital = 100 # in dollars
+    startingCapital = 1000
+    capital = 1000 # in dollars
 
     x = np.linspace(0, len(closePrices), len(closePrices), True)
 
@@ -54,30 +51,27 @@ while True:
         y3 += [previousSignal]
         riskPerTrade = capital * risk * 0.01
 
-        if price > high: high = price
+        if highPrices[i] > high: high = highPrices[i]
 
         if i > 50 and action != 0:
-            relativeStrengthIndex = rsi(closePrices[i - 15 : i])
-            atrResults = atr(highPrices[i - 15 : i], lowPrices[i - 15 : i], closePrices[i - 16 : i - 1])
+            #relativeStrengthIndex = rsi(closePrices[i - 15 : i])
+            #atrResults = atr(highPrices[i - 15 : i], lowPrices[i - 15 : i], closePrices[i - 16 : i - 1])
 
-            if (action == 1 and hasBought and relativeStrengthIndex > 70) or (stop >= price and hasBought):
+            if (action == 1 or price <= stop) and hasBought:
                 ax.scatter(i, price, color='red',marker='v')
-                totalChange += shares * price - shares * buyPrice
-                capital += shares * price 
+                capital += shares * price - shares * buyPrice
                 hasBought = False
                 buyPrice = 0
-            elif action == 2 and (not hasBought) and relativeStrengthIndex < 30:
+            elif action == 2 and (not hasBought):
                 ax.scatter(i, price, color='green',marker='^')
-                shares = min(riskPerTrade / (2 * atrResults), capital/price)
-                #shares = capital / price
-                print(shares)
-                capital -= shares * price
-                stop = max(stop, price - 2 * atrResults)
+                shares = capital / price
+                stop = max(stop, price * 0.95)
                 hasBought = True
                 buyPrice = price
 
         i += 1
 
+    totalChange = capital - startingCapital
 
     print("Stock: ", stockSymbol)
     print("Percent Gain in Asset Value: ", f"{(100 * totalChange / startingCapital):.2f}", "%")
@@ -86,5 +80,3 @@ while True:
     ax.plot(x, y2)
     ax.plot(x, y3)
     plt.show()
-
-
