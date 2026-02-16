0a5895018b395bc8bcca1754ef3c2c2d4d7445a8
hoytdarnell437-source <hoytdarnell437@gmail.com>
2026-02-16 15:02:22 -0500

    add macd lines and legend

--- src/main.py
+++ src/main.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 import numpy as np 
 
 # import other files
-from algorithms.macd import macd
+from algorithms.macd import macd, signalList, macdList
 from algorithms.rsi import rsi
 from trade import trade
 
@@ -14,11 +14,11 @@ while True:
 
     # get input from user
     stockSymbol = input("Input stock symbol: ").upper()
-    algorithm = input("Input Algorithm Key (0: macd, 1: rsi): ")
+    algorithm = int(input("Input Algorithm Key (macd: 0, rsi: 1): "))
     
     # import stock data
     ticker = yf.Ticker(stockSymbol)
-    data = ticker.history(start = "2025-01-11", interval = "1h") # date to start aquiring data and time interval between samples
+    data = ticker.history(start = "2025-01-11", interval = "1d") # date to start aquiring data and time interval between samples
     closePrices = data["Close"].tolist()
 
 
@@ -40,8 +40,9 @@ while True:
     # main calculation loop
     for price in prices:
         # determine what the user wishes to test
+        action = 0
         if algorithm == 0:
-            print("macd")
+            action = macd(price, 1)
         elif algorithm == 1:
             print("rsi")
 
@@ -59,5 +60,16 @@ while True:
     print("Stock: ", stockSymbol)
     print("Percent Gain in Asset Value: ", f"{(100 * totalChange / startingCapital):.2f}", "%")
     print("Percent Gain in Stock Value: ", f"{(100 * (prices[-1] - prices[0]) / prices[0]):.2f}", "%")
-    ax.plot(x, prices)
+    ax.plot(x, prices, label="prices") # plot stock prices
+    if algorithm == 0: # plot macd and signal
+        ax.plot(x, macdList, label="macd")
+        macdList = []
+        ax.plot(x, signalList, label="signal")
+        signalList = []
+    if algorithm == 1: # plot rsi
+        ax.plot()
+    if algorithm == 2: # plot aroon
+        ax.plot()
+    ax.grid(True)
+    plt.legend()
     plt.show()
