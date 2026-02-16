1b32128fad6f2a6c8f7c0ae338fa274d7c231d6e
hoytdarnell437-source <hoytdarnell437@gmail.com>
2026-02-16 14:05:49 -0500

    update .gitignor and README.md

--- tests/algorithms/testATR.py
+++
@@ -1,7 +0,0 @@
-def atr(high: list[float], low: list[float], close: list[float]):
-
-    sumTR = 0
-    period = len(high)
-    for h, l, c in zip(high, low, close):
-        sumTR += max(h - l, abs(h - c), abs(l - c))
-    return sumTR / period
